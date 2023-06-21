996cb3d55a862601a353bf77183126bbd648731d
katerynazinchuk <zinchuck.k@gmail.com>
2023-06-21 19:22:39 +0200

    cleaned file

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <h1 className="App-header">Visual History Ukraine</h1>
+      <button>Home</button>
+    </div>
+  );
+};
+
+export default Header;
