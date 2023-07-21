3d8cf9c01a5c46ccd48a45b852a6f440a086575f
katerynazinchuk <zinchuck.k@gmail.com>
2023-07-21 17:24:40 +0200

    create and style Button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Button.scss";
+
+const Button = () => {
+  return (
+    <button className="home-button">
+      <a className="link" href="#">
+        Main
+      </a>
+    </button>
+  );
+};
+
+export default Button;
