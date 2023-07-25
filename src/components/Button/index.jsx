0a6bda0a0fdc242c6f0c01c6787b8197714eedfa
katerynazinchuk <zinchuck.k@gmail.com>
2023-07-25 16:59:03 +0200

    change button class name and add propTypes

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./Button.scss";
+import PropTypes from "prop-types";
 
-const Button = () => {
+const Button = ({ type, size }) => {
   return (
-    <button className="home-button">
+    <button className="button">
       <a className="link" href="#">
         Main
       </a>
     </button>
   );
+};
+
+Button.propTypes = {
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  type: PropTypes.oneOf(["default", "primary", "danger", "secondary"]),
 };
 
 export default Button;
