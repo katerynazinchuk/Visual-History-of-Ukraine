8d6bb3583ea7c9e724a469f7ed8edb1ccccdc51e
katerynazinchuk <zinchuck.k@gmail.com>
2023-07-28 09:26:26 +0200

    change header structure

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,8 +5,11 @@ import "./Header.scss";
 const Header = () => {
   return (
     <div className="container">
-      <h1 className="main-title">Visual History Ukraine</h1>
-      <Button />
+      <h1>Visual History Ukraine</h1>
+      <Button type={"default"} size={"small"} label={"default"} />
+      <Button type={"prime"} size={"medium"} label={"prime"} />
+      <Button type={"danger"} size={"large"} label={"danger"} />
+      <Button type={"secondary"} size={"small"} label={"secondary"} />
     </div>
   );
 };
