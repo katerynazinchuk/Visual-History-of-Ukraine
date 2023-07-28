43c606e64674e98d07ffbd9dd7df36f21f34b0a9
katerynazinchuk <zinchuck.k@gmail.com>
2023-07-28 09:27:26 +0200

    add icon and label prop to Button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Button.scss";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
 
-const Button = ({ type, size }) => {
+const Button = ({ type, size, icon, label }) => {
+  const classNames = `button ${type} ${size}`;
+
   return (
-    <button className="button">
-      <a className="link" href="#">
-        Main
-      </a>
+    <button className={classNames}>
+      {icon && (
+        <span className="button-icon">
+          <FontAwesomeIcon icon={faPen} />
+        </span>
+      )}
+      {label}
     </button>
   );
 };
@@ -15,6 +22,7 @@ const Button = ({ type, size }) => {
 Button.propTypes = {
   size: PropTypes.oneOf(["small", "medium", "large"]),
   type: PropTypes.oneOf(["default", "primary", "danger", "secondary"]),
+  children: PropTypes.node,
 };
 
 export default Button;
