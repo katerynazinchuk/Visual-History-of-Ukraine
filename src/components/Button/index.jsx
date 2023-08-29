86bb513a297e3e3adad7fab73cbf92c8b2212211
katerynazinchuk <zinchuck.k@gmail.com>
2023-08-29 14:59:34 +0200

    add onClick function to button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,11 +3,11 @@ import "./Button.scss";
 import PropTypes from "prop-types";
 import Icon from "../Icon";
 
-const Button = ({ type, size, icon, label }) => {
+const Button = ({ type, size, icon, label, handleClick }) => {
   const classNames = `button ${type} ${size}`;
 
   return (
-    <button className={classNames}>
+    <button className={classNames} onClick={handleClick}>
       {icon && <Icon name={icon} />}
       {label && <span>{label}</span>}
     </button>
@@ -19,6 +19,7 @@ Button.propTypes = {
   type: PropTypes.oneOf(["default", "primary", "danger", "secondary"]),
   icon: PropTypes.string,
   label: PropTypes.string,
+  handleClick: PropTypes.func,
 };
 
 export default Button;
