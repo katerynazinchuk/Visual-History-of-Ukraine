f87dfbd1e30836fbce2d89ab70767be371c15684
katerynazinchuk <zinchuck.k@gmail.com>
2023-08-29 14:58:57 +0200

    create constants of map historical periods

---
+++ src/components/Map/constants.js
@@ -0,0 +1,22 @@
+export const HISTORICAL_PERIODS = {
+  ANCIENT: {
+    name: "ANCIENT",
+    url: "ancient.jpg",
+  },
+  MEDIEVAL: {
+    name: "MEDIEVAL",
+    url: "medieval.jpg",
+  },
+  RENAISSANCE: {
+    name: "RENAISSANCE",
+    url: "renaissance.jpg",
+  },
+  INDUSTRIAL: {
+    name: "INDUSTRIAL",
+    url: "industrial.jpg",
+  },
+  MODERN: {
+    name: "MODERN",
+    url: "modern.jpg",
+  },
+};
