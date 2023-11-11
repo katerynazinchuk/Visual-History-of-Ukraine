066c47aa37f37737ce3dd6798a85c9c9b0aee855
Orest Lyubinets <orestlog@ukr.net>
2023-11-12 00:53:34 +0100

    make slider style simpler

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,38 +1,68 @@
 import ReactSlider from "react-slider";
 import "./Slider.scss";
 import { PERIODS_ARRAY } from "../Home";
+import { useCallback, useRef, useState } from "react";
 
 const Slider = () => {
-  const makeMark = (props: any) => {
-    console.log("Mark props", props);
+  const maxMarks = 4;
+  const sliderRef = useRef<HTMLDivElement>(null);
+  const marksRefsArray = useRef<HTMLDivElement[]>([]);
+  const markSpreadWidth = sliderRef.current
+    ? sliderRef?.current?.clientWidth / maxMarks
+    : 0;
+
+  const renderMarkElement = (props: any) => {
+    const textShiftMeasurement =
+      marksRefsArray.current[props.key]?.getBoundingClientRect().width / 2;
 
     return (
-      <div className="slider-mark" style={props.style}>
-        <div className="slider-mark-text">{PERIODS_ARRAY[props.key]}</div>
+      <div style={{ position: "absolute", left: props.key * markSpreadWidth }}>
+        <div className="slider-mark" key={props.key + "mark"}></div>
+        <span
+          className="slider-mark-text"
+          style={{ right: textShiftMeasurement }}
+          ref={(element: HTMLDivElement) =>
+            marksRefsArray.current.push(element)
+          }
+        >
+          {PERIODS_ARRAY[props.key]}
+        </span>
+      </div>
+    );
+  };
+
+  const renderThumbElement = (props: any, state: any) => {
+    return (
+      <div
+        {...props}
+        style={{
+          ...props.style,
+          left: props["aria-valuenow"] * markSpreadWidth - 10,
+        }}
+      >
+        <span className="slider-thumb-text">
+          {PERIODS_ARRAY[state.valueNow]}
+        </span>
       </div>
     );
   };
 
   return (
     <div className="slider-container">
-      <ReactSlider
-        className="slider--horisontal"
-        defaultValue={2}
-        marks
-        markClassName="slider-mark"
-        min={0}
-        max={4}
-        thumbClassName="slider-thumb"
-        trackClassName="slider-track"
-        renderThumb={(props, state) => (
-          <div {...props}>
-            <span className="slider-thumb-text">
-              {PERIODS_ARRAY[state.valueNow]}
-            </span>
-          </div>
-        )}
-        renderMark={makeMark}
-      />
+      <div className="slider-wrapper" ref={sliderRef}>
+        <ReactSlider
+          className="slider--horisontal"
+          defaultValue={2}
+          marks
+          markClassName="slider-mark"
+          min={0}
+          max={maxMarks}
+          thumbClassName="slider-thumb"
+          trackClassName="slider-track"
+          renderThumb={renderThumbElement}
+          renderMark={renderMarkElement}
+        />
+      </div>
     </div>
   );
 };
